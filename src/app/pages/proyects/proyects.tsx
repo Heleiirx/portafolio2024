fc104c42d967b1f7a9e9c5e1d0f517f01afd2a35
Itzel <itzelvargas2002@gmail.com>
2024-05-28 20:33:36 -0600

    Adición de Nooks a proyects.tsx

--- src/app/pages/proyects/proyects.tsx
+++ src/app/pages/proyects/proyects.tsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/banner";
 import Encriptador from "../encriptador";
+import Nooks from "../nooks";
 
 export default function Proyects(){
 
@@ -7,6 +8,7 @@ export default function Proyects(){
         <div className="flex flex-col h-fit items-center justify-center relative">
             <Banner bannerText=' ☆ MIS PROYECTOS' background='bg-primary'></Banner>
             <Encriptador/>
+            <Nooks/>
         </div>
     );
 }
