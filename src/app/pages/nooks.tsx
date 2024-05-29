4284143164ff63827e64d287e7e3b5a0681215af
Itzel <itzelvargas2002@gmail.com>
2024-05-29 17:52:52 -0600

    Adicion de estilos h1 y article nooks

--- src/app/pages/nooks.tsx
+++ src/app/pages/nooks.tsx
@@ -4,7 +4,6 @@ import { proyectsArr } from "./proyects/proyectsContext";
 import ProyectColors from "../components/proyectColors";
 import ProyectLinks from "../components/proyectLinks";
 import ProyectSkills from "../components/proyectSkills";
-import hsDesktop from "@/img/hstubeDesktop.png";
 
 export default function Nooks(){
     const proyects = useContext(proyectsArr);
@@ -13,13 +12,16 @@ export default function Nooks(){
     const primaryColor:string = proyects[1].primaryColor;
 
     return(
-        <div>
-            <h1>HStube</h1>
-            <article>Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
-            <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={nooks.primaryColor}  />
-            <ProyectColors colors={colores} primaryColor={primaryColor} />
-            <Image src={hsDesktop} alt="HStube Desktop"></Image>
-            <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor}/>
+        <div className="h-screen relative pt-16">
+            <div className="relative flex flex-col items-end pr-10">
+                <h1 className="w-1/2 text-5xl lg:text-6xl text-right" style={{color:`#${primaryColor}`}}>Nook's Shop</h1>
+                <article className="text-right w-3/4">Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
+                <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor}  />
+            </div>
+            <div className="relative flex flex-col">
+                <ProyectColors colors={colores} primaryColor={primaryColor} />
+                <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor}/>
+            </div>
         </div>
     );
 };
