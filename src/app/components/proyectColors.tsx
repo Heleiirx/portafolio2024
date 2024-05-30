05a6b02a22c682a1e513bb9b45c14a2df0261221
Itzel <itzelvargas2002@gmail.com>
2024-05-30 16:13:24 -0600

    Camnbio de tamaño titulo componente colors

--- src/app/components/proyectColors.tsx
+++ src/app/components/proyectColors.tsx
@@ -4,7 +4,7 @@ import { MyButton } from "./MyButton";
 export default function ProyectColors({colors, primaryColor, classNameParent, classNameChild}:any){
     return(
         <div className={`lg:flex lg:items-center lg:gap-6 ${classNameParent}`}>
-            <h3 className="lg:text-3xl" style={{color:`#${primaryColor}`}}>Colores</h3>
+            <h3 className="text-2xl lg:text-3xl" style={{color:`#${primaryColor}`}}>Colores</h3>
             <div className={`flex gap-4 items-center lg:flex-row lg:gap-7 ${classNameChild}`}>
                 {
                     colors.map((item:any, index:number)=>{
