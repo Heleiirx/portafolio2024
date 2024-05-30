f958fe8931f77097517f92174ccf029e951856c8
Itzel <itzelvargas2002@gmail.com>
2024-05-30 16:10:20 -0600

    Correccion de position de componente colors en encriptador

--- src/app/pages/nooks.tsx
+++ src/app/pages/nooks.tsx
@@ -14,10 +14,10 @@ export default function Nooks(){
         <div className="h-screen relative pt-16">
             <div className="relative flex flex-col items-end pr-10">
                 <h1 className="w-1/2 text-5xl lg:text-6xl text-right" style={{color:`#${primaryColor}`}}>Nooks Shop</h1>
-                <article className="text-right w-3/4">Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
+                <article className="text-right w-3/4 lg:w-1/3">Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
                 <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor}  />
             </div>
-            <div className="relative flex flex-col">
+            <div className="relative flex flex-col-reverse gap-4 h-fill">
                 <ProyectColors colors={colores} primaryColor={primaryColor} />
                 <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor}/>
             </div>
