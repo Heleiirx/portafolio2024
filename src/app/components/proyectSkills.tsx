f4c59f787c431bde380caf84a721afe2f9ccb525
Itzel <itzelvargas2002@gmail.com>
2024-05-30 17:26:16 -0600

    Adición de estilos mobile para nooks

--- src/app/components/proyectSkills.tsx
+++ src/app/components/proyectSkills.tsx
@@ -2,9 +2,9 @@ import { MyChip } from "./MyChip";
 
 export default function ProyectSkills(props:any){
     return(
-     <div className={`flex flex-col w-screen ${props.className}`}>
+     <div className={`flex flex-col w-fill  ${props.className}`}>
         <h3 className="text-2xl mb-2 lg:text-3xl lg:mb-4" style={{color:`#${props.bgcolor}`}}>Herramientas</h3>
-        <div className="flex gap-3 flex-wrap justify-start">
+        <div className="flex gap-3 flex-wrap ">
             {
                 props.skills.map((skill:any, index:number)=>{
                     return <MyChip size="sm" key={index} className={`bg-[#${props.bgcolor}] text-background tracking-wider`} >{skill}</MyChip>
--- src/app/pages/nooks.tsx
+++ src/app/pages/nooks.tsx
@@ -3,6 +3,10 @@ import { proyectsArr } from "./proyects/proyectsContext";
 import ProyectColors from "../components/proyectColors";
 import ProyectLinks from "../components/proyectLinks";
 import ProyectSkills from "../components/proyectSkills";
+import Image from "next/image";
+import nm1 from '@/img/nooksMob1.png';
+import nm2 from '@/img/nooksMob2.png';
+import nm3 from '@/img/nooksMob3.png';
 
 export default function Nooks(){
     const proyects = useContext(proyectsArr);
@@ -11,15 +15,20 @@ export default function Nooks(){
     const primaryColor:string = proyects[1].primaryColor;
 
     return(
-        <div className="h-screen relative pt-16">
-            <div className="relative flex flex-col items-end pr-10">
-                <h1 className="w-1/2 text-5xl lg:text-6xl text-right" style={{color:`#${primaryColor}`}}>Nooks Shop</h1>
-                <article className="text-right w-3/4 lg:w-1/3">Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
-                <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor}  />
+        <div className="h-screen relative pt-20 h-screen flex flex-col items-center">
+            <div className="relative flex flex-col pr-10 gap-4 w-screen">
+                <h1 className="w-1/2 text-5xl lg:text-6xl text-right self-end" style={{color:`#${primaryColor}`}}>Nooks Shop</h1>
+                <article className="text-right w-3/4 lg:w-1/3 self-end">Aplicación web de streaming que reune videos de youtube en una API rest diseñada con Json-Server, permite añadir videos, eliminarlos y editar la información añadida.</article>
+                <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor} className="justify-evenly w-screen" />
             </div>
-            <div className="relative flex flex-col-reverse gap-4 h-fill">
-                <ProyectColors colors={colores} primaryColor={primaryColor} />
-                <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor}/>
+            <div className="flex w-5/6 justify-center gap-2 mt-16">
+                <Image src={nm1} className="w-1/3 translate-y-12" alt="Screenshot pagina principal nooks"></Image>
+                <Image src={nm2} className="w-1/3" alt="Screenshot productos nooks"></Image>
+                <Image src={nm3} className="w-1/3 translate-y-12" alt="Screenshot producto nooks"></Image>
+            </div>
+            <div className="absolute flex flex-col-reverse gap-4 h-fill items-center bottom-14 bg-background py-4">
+                <ProyectColors colors={colores} primaryColor={primaryColor} classNameParent="gap-4"/>
+                <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor} className="mx-5 my-10 gap-4" />
             </div>
         </div>
     );
