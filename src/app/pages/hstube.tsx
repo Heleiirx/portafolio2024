f2862684129d270e7f7eb674400d1a894e4b9318
Itzel <itzelvargas2002@gmail.com>
2024-06-05 13:48:45 -0600

    Responsividad hstube

--- src/app/pages/hstube.tsx
+++ src/app/pages/hstube.tsx
@@ -13,16 +13,16 @@ export default function HStube(){
     const primaryColor:string = hstube.primaryColor;
 
     return(
-        <div className="pt-6" >
-            <div className="flex flex-col px-12 gap-5 z-10">
+        <div className="pt-6 relative h-screen lg:flex" >
+            <div className="flex flex-col px-12 gap-5 lg:basis-2/3 lg:gap-3 lg:pb-4 lg:justify-evenly">
                 <h1 className='text-5xl lg:text-6xl' style={{color:`#${primaryColor}`}} >{hstube.title}</h1>
                 <article className="font-mono text-lg lg:text-xl">{hstube.description}</article>
-                <ProyectLinks page={hstube.pageURL} repo={hstube.repoURL} bgcolor={primaryColor} className='flex-col self-end translate-y-10 gap-6' />
+                <ProyectLinks page={hstube.pageURL} repo={hstube.repoURL} bgcolor={primaryColor} className='flex-col self-end translate-y-10 gap-6 lg:flex-row lg:self-start lg:translate-y-0 lg:justify-evenly lg:w-full' />
+                <Image src={hsDesk} alt='Screenshot HStube desktop' className="-translate-x-20 z-0 -translate-y-20 lg:-translate-x-0 lg:-translate-y-0 lg:w-9/12" />
             </div>
-            <Image src={hsDesk} alt='Screenshot HStube desktop' className="-translate-x-20 z-0 -translate-y-20" />
-            <div className="flex flex-col items-center gap-5 -translate-y-32">
-                <ProyectColors colors={colors} primaryColor={primaryColor} classNameParent="translate-x-20 -translate-y-6 gap-3" classNameChild="" />
-                <ProyectSkills skills={hstube.skills} bgcolor={primaryColor} className="mx-5 gap-5"/>
+            <div className="flex flex-col items-center gap-5 -translate-y-32 lg:basis-1/3 lg:flex-col-reverse lg:justify-items-end lg:-translate-y-0 lg:-translate-x-24 ">
+                <ProyectColors colors={colors} primaryColor={primaryColor} classNameParent="translate-x-20 -translate-y-6 gap-3 lg:translate-x-0 lg:-translate-y-0 lg:pb-4" classNameChild="" />
+                <ProyectSkills skills={hstube.skills} bgcolor={primaryColor} className="mx-5 gap-5 lg:w-96 "/>
             </div>
         </div>
     );
