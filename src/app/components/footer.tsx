8bf331ae14563c8c86ebf4d756313f6f5ea7af9c
Itzel <itzelvargas2002@gmail.com>
2024-06-05 16:58:02 -0600

    Adición de footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return(
+        <div className="h-12 w-screen bg-secondary">
+            <h1 className="text-center py-3 text-background tracking-widest">ITZEL ROMERO | 2024</h1>
+        </div>
+    )
+}
