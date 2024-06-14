2d43168e089cdb56dbdd28a6709a9dfed247484e
Itzel <itzelvargas2002@gmail.com>
2024-06-13 18:34:15 -0600

    Correccion de width img en contacto

--- src/app/pages/contacto.tsx
+++ src/app/pages/contacto.tsx
@@ -29,9 +29,9 @@ export default function Contacto(){
                 <IoMdStar className='absolute text-xl left-14 top-64 z-10 text-4xl lg:left-1/3 lg:top-1/3'/>
                 <IoMdStarOutline className='absolute text-2xl right-14 top-64 z-10 text-4xl lg:right-1/3 lg:top-1/4' />
             </div>
-            <Image src={gracias1} alt='' className='w-2/3 absolute lg:w-1/4'></Image>
-            <Image id='gracias1' src={gracias2} alt='' className='w-2/3 absolute lg:w-1/4'></Image>
-            <Image id='gracias2' src={gracias2} alt='' className='w-2/3 absolute lg:w-1/4'></Image>
+            <Image src={gracias1} alt='' className='w-2/3 absolute lg:w-1/3'></Image>
+            <Image id='gracias1' src={gracias2} alt='' className='w-2/3 absolute lg:w-1/3'></Image>
+            <Image id='gracias2' src={gracias2} alt='' className='w-2/3 absolute lg:w-1/3'></Image>
         </div>
     )
 }
