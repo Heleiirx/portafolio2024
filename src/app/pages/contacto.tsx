2db45fa11d8caa0acba93abcad000255a24545b5
Itzel <itzelvargas2002@gmail.com>
2024-06-13 18:24:01 -0600

    Correccion de posicion de estrellas en contacto desktop

--- src/app/pages/contacto.tsx
+++ src/app/pages/contacto.tsx
@@ -25,9 +25,9 @@ export default function Contacto(){
     return(
         <div ref={scope} className="relative h-screen flex justify-center items-start pt-60 lg:pt-40">
             <div>
-                <IoMdStar className='absolute text-2xl left-12 top-52 z-10 text-4xl star'/>
-                <IoMdStar className='absolute text-xl left-14 top-64 z-10 text-4xl'/>
-                <IoMdStarOutline className='absolute text-2xl right-14 top-64 z-10 text-4xl' />
+                <IoMdStar className='absolute text-2xl left-12 top-52 z-10 text-4xl star lg:left-1/3 lg:top-1/4'/>
+                <IoMdStar className='absolute text-xl left-14 top-64 z-10 text-4xl lg:left-1/3 lg:top-1/3'/>
+                <IoMdStarOutline className='absolute text-2xl right-14 top-64 z-10 text-4xl lg:right-1/3 lg:top-1/4' />
             </div>
             <Image src={gracias1} alt='' className='w-2/3 absolute lg:w-1/4'></Image>
             <Image id='gracias1' src={gracias2} alt='' className='w-2/3 absolute lg:w-1/4'></Image>
