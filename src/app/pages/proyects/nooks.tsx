7ec4149924171a3d98b473d451af052d6f439fe1
Itzel <itzelvargas2002@gmail.com>
2024-10-16 11:22:16 -0600

    Corrección de posicion de carousel es desktop nooks

--- src/app/pages/proyects/nooks.tsx
+++ src/app/pages/proyects/nooks.tsx
@@ -16,11 +16,13 @@ export default function Nooks(){
     const colores:object[] = proyects[1].colors;
     const primaryColor:string = proyects[1].primaryColor;
 
-    return(<>
-     <XCarousel imagenesArr={NooksArr} direccion className="-rotate-12 -translate-y-4 lg:-translate-x-1/4 relative" ></XCarousel>
-     <XCarousel imagenesArr={NooksArr} className="-rotate-12 lg:-translate-x-1/2 absolute" ></XCarousel>
-    <div className="h-screen relative h-screen flex flex-col items-center overflow-y-hidden">
-        <div className="relative flex flex-col pr-10 gap-4 w-screen z-10">
+    return(<div className="relative">
+    <div className="absolute w-screen h-fit lg:translate-y-1/3">
+     <XCarousel imagenesArr={NooksArr} direccion className="hidden lg:inline lg:translate-y-8 lg:-translate-x-1/3 lg:absolute" ></XCarousel>
+     <XCarousel imagenesArr={NooksArr} className="hidden lg:inline lg:-translate-x-1/2 lg:translate-y-40 lg:absolute" ></XCarousel>
+    </div>
+    <div className="h-screen relative flex flex-col items-center overflow-y-hidden">
+        <div className="relative flex flex-col pr-10 gap-4 w-screen z-10 lg:mt-16">
             <h1 className="w-1/2 text-5xl lg:text-6xl text-right self-end" style={{color:`#${primaryColor}`}}>{nooks.title}</h1>
             <article className="text-right w-3/4 self-end font-mono text-lg lg:text-xl lg:w-1/2">{nooks.description}</article>
             <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor} className="justify-evenly w-screen lg:w-2/5 lg:self-end lg:gap-1" />
@@ -30,12 +32,12 @@ export default function Nooks(){
             <Image src={nm2} className="w-1/3" alt="Screenshot productos nooks"></Image>
             <Image src={nm3} className="w-1/3 translate-y-12" alt="Screenshot producto nooks"></Image>
         </div>
-        <div className="absolute flex flex-col-reverse gap-4 h-fill items-center bottom-14 bg-background py-4 lg:flex-row-reverse lg:items-center lg:w-screen lg:justify-evenly lg:bottom-1/3 lg:py-0 lg:h-fit">
+        <div className="absolute flex flex-col-reverse gap-4 h-fill items-center bottom-14 bg-background py-4 lg:flex-row-reverse lg:items-center lg:w-screen lg:justify-evenly lg:bottom-1/4 lg:py-0 lg:h-fit">
             <ProyectColors colors={colores} primaryColor={primaryColor} classNameParent="gap-4 lg:p-2"/>
             <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor} className="mx-5 my-4 gap-4 lg:self-start lg:w-1/2 lg:my-0 lg:p-2" />
         </div>
     </div>
-    </>
+    </div>
            
     );
 };
