5a010ff43f279a48b455957a3adaba917e060683
Itzel <itzelvargas2002@gmail.com>
2025-08-01 12:36:58 -0600

    Add toolsData module and Procreate icon for tool display

---
+++ src/app/contexts/toolsData.tsx
@@ -0,0 +1,53 @@
+import { FaGitAlt } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiPostman } from "react-icons/si";
+import { FaDocker } from "react-icons/fa6";
+import { FaFigma } from "react-icons/fa";
+
+
+
+export const toolsData = [
+  {
+    id: 1,
+    name: 'Git',
+    icon: <FaGitAlt />, 
+    color: '#EC4899'
+  },
+  {
+    id: 2,
+    name: 'Github',
+    icon: <FaGithub/>,
+    color: '#EC4899'
+  },
+  {
+    id: 3,
+    name: 'MySQL',
+    icon: <GrMysql />,
+    color: '#EC4899'
+  },
+  {
+    id: 4,
+    name: 'Postman',
+    icon: <SiPostman />,
+    color: '#EC4899'
+  },
+  {
+    id: 5,
+    name: 'Docker',
+    icon: <FaDocker />,
+    color: '#EC4899'
+  },
+  {
+    id: 6,
+    name: 'Figma',
+    icon: <FaFigma />,
+    color: '#EC4899'
+  },
+//   {
+//     id: 7,
+//     name: 'Procreate',
+//     icon: '🗄️',
+//     color: '#EC4899'
+//   }
+];
