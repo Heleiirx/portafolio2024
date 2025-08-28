bcfe5b5b75cd6d35ef09c1c39688d11919d95e75
Itzel <itzelvargas2002@gmail.com>
2025-08-28 14:20:37 -0600

    Update ProyectSkills component header and adjust MyChip styling

--- src/app/components/proyectSkills.tsx
+++ src/app/components/proyectSkills.tsx
@@ -9,11 +9,11 @@ interface proyectSkillsProps{
 export default function ProyectSkills({className, bgcolor, skills}:proyectSkillsProps){
     return(
      <div className={`flex flex-col w-fill  ${className}`}>
-        <h3 className="text-2xl mb-2 lg:text-3xl lg:mb-4" style={{color:`#${bgcolor}`}}>Herramientas</h3>
+        <h3 className="text-xl mb-2 lg:text-3xl lg:mb-4" style={{color:`#${bgcolor}`}}>Stack</h3>
         <div className="flex gap-3 flex-wrap ">
             {
                 skills.map((skill:any, index:number)=>{
-                    return <MyChip size="sm" key={index} className={`bg-[#${bgcolor}] text-background tracking-wider`} >{skill}</MyChip>
+                    return <MyChip size="sm" key={index} style={{backgroundColor:`#${bgcolor}`}} className={`text-background tracking-wider`} >{skill}</MyChip>
                 })
             }
         </div>
