b85d0e44cd4efc3ece1c03fac7f663d2bc43ba87
Itzel <itzelvargas2002@gmail.com>
2025-08-31 17:42:54 -0600

    Refactor HStube component layout and integrate VerticalCarousel for enhanced image display

--- src/app/pages/proyects/hstube.tsx
+++ src/app/pages/proyects/hstube.tsx
@@ -5,24 +5,35 @@ import ProyectLinks from "../../components/proyectLinks";
 import ProyectSkills from "../../components/proyectSkills";
 import { proyectsArr } from "../../contexts/proyectsContext";
 import hsDesk from '@/img/hstubeDesktop.png';
+import VerticalCarousel from "../../components/VerticalCarousel";
+import { HstubeArr } from "../../contexts/carouselArr";
 
 export default function HStube(){
     const proyects = useContext(proyectsArr);
-    const hstube = proyects[2];
+    const hstube = proyects[3];
     const colors:object[] = hstube.colors;
     const primaryColor:string = hstube.primaryColor;
 
     return(
-        <div className="pt-6 relative h-screen lg:flex" >
-            <div className="flex flex-col px-12 gap-5 lg:basis-2/3 lg:gap-3 lg:pb-4">
-                <h1 className='text-5xl lg:text-6xl' style={{color:`#${primaryColor}`}} >{hstube.title}</h1>
-                <article className="font-mono text-lg lg:text-xl">{hstube.description}</article>
-                <ProyectLinks page={hstube.pageURL} repo={hstube.repoURL} figma={hstube.figmaURL} bgcolor={primaryColor} className='flex-col self-end translate-y-10 gap-6 lg:flex-row lg:self-start lg:translate-y-0 lg:justify-evenly lg:w-full' />
-                <Image src={hsDesk} alt='Screenshot HStube desktop' className="-translate-x-20 z-0 -translate-y-20 lg:-translate-x-0 lg:-translate-y-0 lg:w-9/12" />
+        <div className="pt-6 relative h-screen lg:flex mt-12" >
+            <div className="flex flex-col items-center">
+                {/* <ProyectColors colors={colors} primaryColor={primaryColor} classNameParent="translate-x-20 -translate-y-6 gap-3 lg:translate-x-0 lg:-translate-y-0 lg:pb-4" classNameChild="" /> */}
+                <div className="flex gap-12 w-1/2 h-96 lg:h-full overflow-hidden">
+                    <VerticalCarousel direccion={true} imagenesArr={HstubeArr} />
+                    <VerticalCarousel imagenesArr={HstubeArr} className="-translate-y-36" />
+                </div>
+                <ProyectSkills skills={hstube.skills} bgcolor={primaryColor} className="mx-5 gap-5 lg:w-96 lg:p-4"/>
             </div>
-            <div className="flex flex-col items-center gap-5 -translate-y-32 lg:basis-1/3 lg:flex-col-reverse lg:justify-items-end lg:-translate-y-0 lg:-translate-x-24 ">
-                <ProyectColors colors={colors} primaryColor={primaryColor} classNameParent="translate-x-20 -translate-y-6 gap-3 lg:translate-x-0 lg:-translate-y-0 lg:pb-4" classNameChild="" />
-                <ProyectSkills skills={hstube.skills} bgcolor={primaryColor} className="mx-5 gap-5 lg:w-96 "/>
+            <div className="flex flex-col px-12 gap-5 lg:basis-2/3 lg:gap-3 lg:pb-4">
+                <h1 className='text-4xl lg:text-5xl font-bold text-white' style={{color:`#${primaryColor}`}} >{hstube.title}</h1>
+                {hstube.type === 'personal' ?  <h5 className="text-md font-bold" >Personal project</h5> : <h5 className="text-md font-bold" >Team project</h5>} 
+                <span className="flex gap-6">
+                    <h5 className="text-md font-bold" >Timeline</h5>
+                    <p className="text-sm font-mono">{hstube.timeline}</p>
+                </span>
+                <article className="text-sm font-mono">{hstube.description}</article>
+                <ProyectLinks page={hstube.pageURL} repo={hstube.repoURL} figma={hstube.figmaURL} bgcolor={primaryColor} className='flex self-center gap-4 my-4' />
+                <Image src={hsDesk} alt='Screenshot HStube desktop' className="-translate-x-20 z-0 -translate-y-20 lg:-translate-x-0 lg:-translate-y-0 lg:w-9/12" />
             </div>
         </div>
     );
