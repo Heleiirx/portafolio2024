e4b2f3be221fda0945c2c62d0c80cfbd9429c23e
Itzel <itzelvargas2002@gmail.com>
2025-09-01 17:31:27 -0600

    Update projectLinks with bilingual translation

--- src/app/components/proyectLinks.tsx
+++ src/app/components/proyectLinks.tsx
@@ -1,5 +1,6 @@
 import {Link} from "@nextui-org/react";
 import { FaExternalLinkAlt, FaGithub, FaFigma } from "react-icons/fa";
+import { useLanguage } from "../contexts/LanguageContext";
 
 interface proyectLinksProps{
     className:string,
@@ -10,19 +11,21 @@ interface proyectLinksProps{
 }
 
 export default function ProyectLinks({className, bgcolor, page, repo, figma}:proyectLinksProps){
+    const {t, language} = useLanguage();
+
    return(
     <div className={`flex flex-wrap ${className} items-center justify-center `}>
     <Link size="lg" href={page} className="lg:text-xl flex gap-1 hover:underline transition-all duration-200 mx-8" style={{color:`#${bgcolor}`}}>
             <FaExternalLinkAlt></FaExternalLinkAlt>
-            Página web
+            {t('projects.demoLink')}
         </Link>
     <Link size="lg" href={repo} className="lg:text-xl flex gap-1 hover:underline transition-all duration-200 mx-8" style={{color:`#${bgcolor}`}}>
             <FaGithub></FaGithub>
-            Repositorio
+            {t('projects.repoLink')}
         </Link>
     { ! figma ? null : ( <Link size="lg" href={figma} className="lg:text-xl flex gap-1 hover:underline transition-all duration-200 mx-8" style={{color:`#${bgcolor}`}}>
             <FaFigma></FaFigma>
-            Diseño Figma
+            {t('projects.figmaLink')}
         </Link> )
         }
         
