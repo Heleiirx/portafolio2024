46fef399b165bc33113a804f20fbd696e58a2406
Itzel <itzelvargas2002@gmail.com>
2025-09-01 17:34:18 -0600

    Update footer date

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer(){
     return(
         <div className="h-12 w-screen bg-secondary font-mono">
-            <h3 className="text-center py-3 text-background tracking-widest">Itzel Romero | Web Developer</h3>
+            <h3 className="text-center py-3 text-background tracking-widest">Itzel Romero | 2024 - 2025</h3>
         </div>
     )
 }
