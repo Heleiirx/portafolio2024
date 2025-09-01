611a5fe4744dbf835aa2ea9f0b683318c343c9e6
Itzel <itzelvargas2002@gmail.com>
2025-09-01 17:31:55 -0600

    Edit unused projects code

--- src/app/pages/proyects/encriptador.tsx
+++ src/app/pages/proyects/encriptador.tsx
@@ -1,32 +1,32 @@
-import React, { useContext } from "react";
-import Image from 'next/image'
-import { proyectsArr } from "../../contexts/proyectsContext";
-import ProyectLinks from "../../components/proyectLinks";
-import ProyectColors from "../../components/proyectColors";
-import ProyectSkills from "../../components/proyectSkills";
-import desktopImg from '@/img/encriptadorDesktop.png';
-import mobileImg from '@/img/encriptadorMovile.png'
+// import React, { useContext } from "react";
+// import Image from 'next/image'
+// import { proyectsArr } from "../../contexts/proyectsContext";
+// import ProyectLinks from "../../components/proyectLinks";
+// import ProyectColors from "../../components/proyectColors";
+// import ProyectSkills from "../../components/proyectSkills";
+// import desktopImg from '@/img/encriptadorDesktop.png';
+// import mobileImg from '@/img/encriptadorMovile.png'
 
-export default function Encriptador(){
-    const proyects = useContext(proyectsArr);
-    const encriptador= proyects[1];
-    const colores:object[] = encriptador.colors;
-    const primaryColor = encriptador.primaryColor;
+// export default function Encriptador(){
+//     const proyects = useContext(proyectsArr);
+//     const encriptador= proyects[1];
+//     const colores:object[] = encriptador.colors;
+//     const primaryColor = encriptador.primaryColor;
 
-    return(
-    <div className="flex flex-col min-h-screen pt-16 relative">
-        <div className="flex flex-col gap-4 items-center relative lg:flex-row lg:justify-evenly lg:items-start">
-            <h1 className="text-5xl lg:text-6xl" style={{color:`#${encriptador.primaryColor}`}}>{encriptador.title}</h1>
-            <p>{encriptador.type}</p>
-            <p>{encriptador.timeline}</p>
-            <article className="font-mono w-3/4 text-lg lg:w-1/2 lg:text-xl lg:pb-14">{encriptador.description}</article>
-            <ProyectLinks page={encriptador.pageURL} repo={encriptador.repoURL} bgcolor={encriptador.primaryColor} className="lg:w-1/2 lg:absolute lg:bottom-0 lg:right-12 justify-evenly w-screen"/>
-        </div>
-        <div className="flex flex-col mt-5 relative lg:-translate-y-12 lg:mt-0">
-            <Image alt="Encriptador Desktop" src={desktopImg} className="translate-x-1/4 h-96 lg:h-auto lg:w-1/2 lg:translate-x-2" ></Image>
-            <ProyectColors colors={colores} primaryColor={encriptador.primaryColor} classNameParent="absolute top-12 left-6 lg:relative lg:top-[12rem] lg:left-14 lg:w-1/4" classNameChild="flex-col mt-3" />
-            <ProyectSkills skills={encriptador.skills} bgcolor={primaryColor} className="p-4 -translate-y-9 lg:-translate-y-24 lg:translate-x-10 lg:flex-wrap lg:mt-4 lg:w-1/2"/>
-        </div>
-        <Image alt="Encriptador Movile" src={mobileImg} className="hidden lg:w-5/12 lg:absolute lg:bottom-0 lg:right-0 lg:inline"></Image>
-    </div>);
-};
+//     return(
+//     <div className="flex flex-col min-h-screen pt-16 relative">
+//         <div className="flex flex-col gap-4 items-center relative lg:flex-row lg:justify-evenly lg:items-start">
+//             <h1 className="text-5xl lg:text-6xl" style={{color:`#${encriptador.primaryColor}`}}>{encriptador.title}</h1>
+//             <p>{encriptador.type}</p>
+//             <p>{encriptador.timeline}</p>
+//             <article className="font-mono w-3/4 text-lg lg:w-1/2 lg:text-xl lg:pb-14">{encriptador.description}</article>
+//             <ProyectLinks page={encriptador.pageURL} repo={encriptador.repoURL} bgcolor={encriptador.primaryColor} className="lg:w-1/2 lg:absolute lg:bottom-0 lg:right-12 justify-evenly w-screen"/>
+//         </div>
+//         <div className="flex flex-col mt-5 relative lg:-translate-y-12 lg:mt-0">
+//             <Image alt="Encriptador Desktop" src={desktopImg} className="translate-x-1/4 h-96 lg:h-auto lg:w-1/2 lg:translate-x-2" ></Image>
+//             <ProyectColors colors={colores} primaryColor={encriptador.primaryColor} classNameParent="absolute top-12 left-6 lg:relative lg:top-[12rem] lg:left-14 lg:w-1/4" classNameChild="flex-col mt-3" />
+//             <ProyectSkills skills={encriptador.skills} bgcolor={primaryColor} className="p-4 -translate-y-9 lg:-translate-y-24 lg:translate-x-10 lg:flex-wrap lg:mt-4 lg:w-1/2"/>
+//         </div>
+//         <Image alt="Encriptador Movile" src={mobileImg} className="hidden lg:w-5/12 lg:absolute lg:bottom-0 lg:right-0 lg:inline"></Image>
+//     </div>);
+// };
--- src/app/pages/proyects/nooks.tsx
+++ src/app/pages/proyects/nooks.tsx
@@ -1,45 +1,45 @@
-import React, {useContext} from "react";
-import { proyectsArr } from "../../contexts/proyectsContext";
-import ProyectColors from "../../components/proyectColors";
-import ProyectLinks from "../../components/proyectLinks";
-import ProyectSkills from "../../components/proyectSkills";
-import Image from "next/image";
-import nm1 from '@/img/nooksMob1.png';
-import nm2 from '@/img/nooksMob2.png';
-import nm3 from '@/img/nooksMob3.png';
-import XCarousel from "@/app/components/XInfiniteCarousel";
-import { NooksArr } from "@/app/contexts/carouselArr";
+// import React, {useContext} from "react";
+// import { proyectsArr } from "../../contexts/proyectsContext";
+// import ProyectColors from "../../components/proyectColors";
+// import ProyectLinks from "../../components/proyectLinks";
+// import ProyectSkills from "../../components/proyectSkills";
+// import Image from "next/image";
+// import nm1 from '@/img/nooksMob1.png';
+// import nm2 from '@/img/nooksMob2.png';
+// import nm3 from '@/img/nooksMob3.png';
+// import XCarousel from "@/app/components/XInfiniteCarousel";
+// import { NooksArr } from "@/app/contexts/carouselArr";
 
-export default function Nooks(){
-    const proyects = useContext(proyectsArr);
-    const nooks = proyects[2];
-    const colores:object[] = proyects[2].colors;
-    const primaryColor:string = proyects[2].primaryColor;
+// export default function Nooks(){
+//     const proyects = useContext(proyectsArr);
+//     const nooks = proyects[2];
+//     const colores:object[] = proyects[2].colors;
+//     const primaryColor:string = proyects[2].primaryColor;
 
-    return(<div className="relative">
-    <div className="absolute w-screen h-fit lg:translate-y-1/3">
-     <XCarousel imagenesArr={NooksArr} direccion className="hidden lg:inline lg:translate-y-8 lg:-translate-x-1/3 lg:absolute" ></XCarousel>
-     <XCarousel imagenesArr={NooksArr} className="hidden lg:inline lg:-translate-x-1/2 lg:translate-y-40 lg:absolute" ></XCarousel>
-    </div>
-    <div className="h-screen relative flex flex-col items-center overflow-y-hidden">
-        <div className="relative flex flex-col pr-10 gap-4 w-screen z-10 lg:mt-16">
-            <h1 className="w-1/2 text-5xl lg:text-6xl text-right self-end" style={{color:`#${primaryColor}`}}>{nooks.title}</h1>
-            <p>{nooks.type}</p>
-            <p>{nooks.timeline}</p>
-            <article className="text-right w-3/4 self-end font-mono text-lg lg:text-xl lg:w-1/2">{nooks.description}</article>
-            <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor} className="justify-evenly w-screen lg:w-2/5 lg:self-end lg:gap-1" />
-        </div>
-        <div className="flex w-5/6 justify-center gap-2 mt-16 lg:translate-y-48 lg:mt-0">
-            <Image src={nm1} className="w-1/3 translate-y-12" alt="Screenshot pagina principal nooks"></Image>
-            <Image src={nm2} className="w-1/3" alt="Screenshot productos nooks"></Image>
-            <Image src={nm3} className="w-1/3 translate-y-12" alt="Screenshot producto nooks"></Image>
-        </div>
-        <div className="absolute flex flex-col-reverse gap-4 h-fill items-center bottom-14 bg-background py-4 lg:flex-row-reverse lg:items-center lg:w-screen lg:justify-evenly lg:bottom-1/4 lg:py-0 lg:h-fit">
-            <ProyectColors colors={colores} primaryColor={primaryColor} classNameParent="gap-4 lg:p-2"/>
-            <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor} className="mx-5 my-4 gap-4 lg:self-start lg:w-1/2 lg:my-0 lg:p-2" />
-        </div>
-    </div>
-    </div>
+//     return(<div className="relative">
+//     <div className="absolute w-screen h-fit lg:translate-y-1/3">
+//      <XCarousel imagenesArr={NooksArr} direccion className="hidden lg:inline lg:translate-y-8 lg:-translate-x-1/3 lg:absolute" ></XCarousel>
+//      <XCarousel imagenesArr={NooksArr} className="hidden lg:inline lg:-translate-x-1/2 lg:translate-y-40 lg:absolute" ></XCarousel>
+//     </div>
+//     <div className="h-screen relative flex flex-col items-center overflow-y-hidden">
+//         <div className="relative flex flex-col pr-10 gap-4 w-screen z-10 lg:mt-16">
+//             <h1 className="w-1/2 text-5xl lg:text-6xl text-right self-end" style={{color:`#${primaryColor}`}}>{nooks.title}</h1>
+//             <p>{nooks.type}</p>
+//             <p>{nooks.timeline}</p>
+//             <article className="text-right w-3/4 self-end font-mono text-lg lg:text-xl lg:w-1/2">{nooks.description}</article>
+//             <ProyectLinks page={nooks.pageURL} repo={nooks.repoURL} bgcolor={primaryColor} className="justify-evenly w-screen lg:w-2/5 lg:self-end lg:gap-1" />
+//         </div>
+//         <div className="flex w-5/6 justify-center gap-2 mt-16 lg:translate-y-48 lg:mt-0">
+//             <Image src={nm1} className="w-1/3 translate-y-12" alt="Screenshot pagina principal nooks"></Image>
+//             <Image src={nm2} className="w-1/3" alt="Screenshot productos nooks"></Image>
+//             <Image src={nm3} className="w-1/3 translate-y-12" alt="Screenshot producto nooks"></Image>
+//         </div>
+//         <div className="absolute flex flex-col-reverse gap-4 h-fill items-center bottom-14 bg-background py-4 lg:flex-row-reverse lg:items-center lg:w-screen lg:justify-evenly lg:bottom-1/4 lg:py-0 lg:h-fit">
+//             <ProyectColors colors={colores} primaryColor={primaryColor} classNameParent="gap-4 lg:p-2"/>
+//             <ProyectSkills  skills={nooks.skills} bgcolor={primaryColor} className="mx-5 my-4 gap-4 lg:self-start lg:w-1/2 lg:my-0 lg:p-2" />
+//         </div>
+//     </div>
+//     </div>
            
-    );
-};
+//     );
+// };
